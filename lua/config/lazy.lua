34eb05891ba1cbfd8ba2f6a089f22ea01a1af188
Hilman Ibnu <hia.develop@gmail.com>
2023-11-14 14:55:06 +0700

    [update] configuration for phpactor and blade

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,14 +14,14 @@ require("lazy").setup({
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    ---   { import = "lazyvim.plugins.extras.ui.mini-animate" },
     ---{ import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     ---{ import = "lazyvim.plugins.extras.lang.tex" },
     { import = "lazyvim.plugins.extras.lang.python" },
     --- import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.dap.core" },
-    ---{ import = "lazyvim.plugins.extras.lsp.none-ls" },
+    { import = "lazyvim.plugins.extras.lsp.none-ls" },
     -- import/override with your plugins
     { import = "plugins" },
   },
